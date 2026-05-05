23b3efb19a8327a9ca62a14daa20b1961c157d9b
mangokiwi02 <imchangsu12@naver.com>
2026-05-05 23:06:39 +0900

    [Passed] 수들의 최대 차 (8ms, 0MB)

--- trail2/수들의 최대 차/maximum-difference-in-numbers.cpp
+++ trail2/수들의 최대 차/maximum-difference-in-numbers.cpp
@@ -12,14 +12,30 @@ int main() {
     sort(arr,arr+n);
     int ans = 0;
     //방벙1_처음 수를 기준으로 탐색 
-    for(int i=0;i<n;i++){
-        int cnt =0;
-        for(int j=i;j<n;j++){
-            if(arr[j] - arr[i] <=k){
-                cnt++;
-            }else break;
+    // for(int i=0;i<n;i++){
+    //     int cnt =0;
+    //     for(int j=i;j<n;j++){
+    //         if(arr[j] - arr[i] <=k){
+    //             cnt++;
+    //         }else break;
+    //     }
+    //     ans = max(cnt,ans);
+    // }
+    // 방법2_ 윈도우를 조절 
+    // for(int i=2; i<n;i++){ // 윈도우 크기 
+    //     for(int j=0;j<n-i;j++){
+    //         if(arr[j+i-1] - arr[j] <=k ){
+    //             ans = max(ans,i);
+    //         }
+    //     }
+    // }
+    //방법3_ 투포인터 방식(ai 참고) 
+    int r = 0;
+    for(int l=0;l<n;l++){
+        while(r<n && arr[r] - arr[l]<=k){ // 이 부분이 중요
+            r++;
         }
-        ans = max(cnt,ans);
+        ans = max(ans,r-l);
     }
     cout << ans;
     return 0;
